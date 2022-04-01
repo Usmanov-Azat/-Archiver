1eae7d4f73c77fac6231f6b423bbe876f7ead004
Usmanov-Azat <azatu421@gmail.com>
2022-04-01 23:59:25 +0300

    refactoring

--- main.c
+++ main.c
@@ -1,45 +1,52 @@
 #include <unistd.h>
-#include <stdio.h>
+#include <stdio.h> 
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
 
+int WriteToFile(int in, int out, char* filepath, struct stat statbuf);
+int archivate(int out, char* dir);
+int StartArchivate();
+int ReadFromFile(int in);
+int ReadCatalog(int in);
+int dearchivate(const char* filepath);
 
-/*
-Структура архива (информация и ее размер)
-Для каталога:
-Признак того, что это каталог - int
-Название каталога - char[255]
-Количество элементов в каталоге - int
+const int FILENAME_SIZE = 255;
 
-Структура файла:
-Признак того, что не каталог - int
-Название файла - char[255]
-Реальный размер файла - int
-Размер файла в условных единицах - int
-Сам файл
 
-На вход архиватора должен подаваться именно каталог
-*/
+int main()
+{
+    int change;
+    printf("Select action:\n");
+    printf("1 - files to archive\n");
+    printf("2 - read archive\n");
+    scanf("%d", &change);
+    if (change == 1)
+        StartArchivate();
+    else if (change == 2)
+        dearchivate("arcive.arc");
+
+    return 0;
+}
 
 int WriteToFile(int in, int out, char* filepath, struct stat statbuf)
 {
-    char buf;
-    char longbuf[255];
+    char buffer;
+    char long_buffer[FILENAME_SIZE];
     int i = 0;
-    int filesize = 0;
     int readsize = 0;
-    int iscat = 0;
+    int filesize = 0;
+    int is_catalog = 0;
 
     //записываем, что это не каталог
-    write(out, &iscat, sizeof(int));
+    write(out, &is_catalog, sizeof(int));    //(дескриптор в кот зап данные, записываемые данные, количество данных)
     //копируем название файла в длинный буфер
-    strcpy(longbuf, filepath);
-    printf("%s\n", longbuf);
+    strcpy(long_buffer, filepath);
+    printf("%s\n", long_buffer);
     //записываем название файла в архив
-    write(out, longbuf, 255);
+    write(out, long_buffer, 255);
 
     //получаем информацию о файле
     filesize = statbuf.st_size; //получаем размер
@@ -52,9 +59,9 @@ int WriteToFile(int in, int out, char* filepath, struct stat statbuf)
     write(out, &readsize, sizeof(int)); //пустышка, сюда будет записан размер в "буферах"
     //записываем файл в архив
 
-    while(read(in, &buf, 1) == 1)
+    while (read(in, &buffer, 1) == 1)
     {
-        write(out, &buf, 1);
+        write(out, &buffer, 1);
         //поскольку у нас одна единица буфера, к сожалению, не равна
         //одному байту, надо считать размер файла в "буферах" и записать
         //перед файлом
@@ -62,7 +69,7 @@ int WriteToFile(int in, int out, char* filepath, struct stat statbuf)
     }
 
     //смещаемся в место, которое после реального размера
-    int a = lseek(out, -filesize-sizeof(int), SEEK_CUR); //в данном случае у нас указатель именно по объему файла,
+    int a = lseek(out, -filesize - sizeof(int), SEEK_CUR); //в данном случае у нас указатель именно по объему файла,
     //записываем сколько надо считать                     //поэтому используем РЕАЛЬНЫЙ вес файла, а НЕ СЧИТАНЫЫЙ
     write(out, &readsize, sizeof(int));
     //возвращаемся в конец
@@ -71,19 +78,17 @@ int WriteToFile(int in, int out, char* filepath, struct stat statbuf)
     return 0;
 }
 
-int arc(int out, char* dir)
+int archivate(int out, char* dir)
 {
-    DIR *dp;
-
+    DIR* dp;//открывает и возвращает поток каталога для чтения каталога
+    struct dirent* entry;//создаем структуру типа вшкуте с названием entry
+    struct stat statbuf;
     int i;
     int in;
-    struct dirent *entry;
-    char namebuf[255];
-    int isdir = 1;
+    char name_buffer[255];//имя файла
+    int is_directory = 1;
     int numoffiles = 0;
-    int numfilepos;
-    struct stat statbuf;
-
+    int numfilepos;  
     int check;
 
 
@@ -93,46 +98,47 @@ int arc(int out, char* dir)
         fprintf(stderr, "cannot open directory: %s\n", dir);
         return 1;
     }
-    strcpy(namebuf, dir);
-    //printf("%s\n", &namebuf);
+    strcpy(name_buffer, dir);//dir копируем namebuf
     //записываем, что это каталог
-    check = write(out, &isdir, sizeof(int));
-    //printf("Bytes written: %d\n", check);
+    check = write(out, &is_directory, sizeof(int));//!!!!!!!
     //записываем название каталога в архив
-    check = write(out, namebuf, 255);
-     //printf("Bytes written: %d\n", check);
-    //пустышка для кол-ва файлов
-    numfilepos = lseek(out, 0, SEEK_CUR);
-    //printf("Current position: %d\n", numfilepos);
-    write(out, &isdir, sizeof(int));
+    check = write(out, name_buffer, 255);
+   //пустое значение для кол-ва файлов
+    numfilepos = lseek(out, 0, SEEK_CUR);//первый параметр куда смещаемся, второй на сколько, третий от куда, SEEK_CUR - текущее местоположение.В этой функции отображаем где бы находимся так как смещаемся на ноль.
+    write(out, &is_directory, sizeof(int));
     //меняем текущий каталог
-    chdir(dir);
-    while((entry = readdir(dp)) != NULL)
+    chdir(dir); //chdir изменяет текущий каталог каталог на dir.
+    while ((entry = readdir(dp)) != NULL) // при каждом вызове в ентри записывает
     {
-        stat(entry->d_name, &statbuf);
+        stat(entry->d_name, &statbuf);//вытащили из ентри название файла в статбав
         //провеврка на каталог
-        if(S_ISDIR(statbuf.st_mode))
+        if (S_ISDIR(statbuf.st_mode))
         {
-            if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
+            if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)//стрсмп сравнивает две строки,  проверкак на еще один каталог
                 continue;
-            printf("%s%s/\n", "", entry->d_name);
-            /* Рекурсивный вызов с новый отступом */
-            arc(out ,entry->d_name);
-            numoffiles++;
+            printf("%s%s/\n", "", entry->d_name);//выводим имя
+            // Рекурсивный вызов с новый отступом 
+            archivate(out, entry->d_name);
+            numoffiles++;//увеличиваем кол-во файлов
         }
         else
         {
             printf("%s\n", entry->d_name);
             //открываем данный файл
             in = open(entry->d_name, O_RDONLY);
+            if (in == -1)
+            {
+                printf("Ошибка при открытии файла!");
+                return 1;
+            }
             stat(entry->d_name, &statbuf);
             WriteToFile(in, out, entry->d_name, statbuf);
             close(in);
             numoffiles++;
         }
     }
-    printf("Num of files: %d\n",  numoffiles);
-    lseek(out, numfilepos,SEEK_SET);
+    printf("Number of files: %d\n", numoffiles);
+    lseek(out, numfilepos, SEEK_SET);
 
     write(out, &numoffiles, sizeof(int));
     lseek(out, 0, SEEK_END);
@@ -141,130 +147,118 @@ int arc(int out, char* dir)
 
     return 0;
 }
-int StartArc()
+int StartArchivate()
 {
-    char* filepath;
-    scanf("%s", &filepath);
+    char file_path[FILENAME_SIZE];//путь до файла
+    scanf("%s", file_path);//ситывание
     //создаем архив
-    int out = open("arcive.arc", O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
-    arc(out, &filepath);
+    int out = open("arcive.arc", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);//O_WRONLY - файл для записи, если файла нет то он будет создан, S_IRUSR - право на чтение, S_IWUSR - право на запись в файл
+    if (out == -1)
+    {
+        printf("Ошибка записи!");
+        return 1;
+    }
+    archivate(out, file_path);
     close(out);
 }
 
 int ReadFromFile(int in)
 {
-    char buf;
-    //char longbuf[255];
+    char buffer;
     int out;
-    int namesize;
-    int filesize = 0;
-    int readsize = 0;
+    int file_size = 0;
+    int read_size = 0;
 
-    char Outfile[255];
+    char Outfile[FILENAME_SIZE];
     struct stat statbuf;
 
     //читаем название файла
-    read(in, &Outfile, 255);
+    read(in, &Outfile, FILENAME_SIZE);//(файл с кот читаем, место хранения, скорлько байт считываем) 0 - считывать нечего, -1 - ошибка при чтении
     //читаем размер файла
-    read(in, &filesize, sizeof(int));
+    read(in, &file_size, sizeof(int));
     //читаем размер для чтения
-    read(in, &readsize, sizeof(int));
+    read(in, &read_size, sizeof(int));
 
     //создаем "разархивированный" файл
-    out = open(Outfile, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
-
-    printf("Name of file %s\n", &Outfile);
-    printf("Size of file %d bytes\n", filesize);
-
-
-
-    for(int i = 0; i < readsize; i++)
+    out = open(Outfile, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    if (out == -1)
     {
-        read(in, &buf, 1);
-        write(out, &buf, 1);
+        printf("Ошибка записи!");
+        return 1;
+    }
+
+    printf("Name of file %s\n", Outfile);
+    printf("Size of file %d bytes\n", file_size);
+
+
+
+    for (int i = 0; i < read_size; i++)
+    {
+        read(in, &buffer, 1);
+        write(out, &buffer, 1);
     }
 
     return 0;
 }
 
-int ReadCat(int in)
+int ReadCatalog(int in)
 {
-    char buf;
-    //char longbuf[255];
-    int out;
     int namesize;
-    int filesize = 0;
-    int readsize = 0;
     int numoffiles;
-    int IsCat = 0;
+    int IsCatalog = 0;
 
     int check;
 
-    char outfile[255];
+    char outfile[FILENAME_SIZE];
     struct stat statbuf;
     //читаем информацию о том, каталог это или нет
-    check = read(in, &IsCat, sizeof(int));
+    check = read(in, &IsCatalog, sizeof(int));
     printf("Bytes read: %d\n", check);
-    printf("Is cat: %d\n", IsCat);
+    printf("Is catalogue: %d\n", IsCatalog);
     //если каталог
-    if(IsCat != 0)
+    if (IsCatalog != 0)
     {
         //считываем название, кол-во файлов
-        check = read(in, outfile, 255);
+        check = read(in, outfile, FILENAME_SIZE);
         printf("Bytes read: %d\n", check);
         check = read(in, &numoffiles, sizeof(int));
         printf("Bytes read: %d\n", check);
-        printf("Num of files: %d\n",  numoffiles);
-        printf("%s\n", &outfile);
+        printf("Number of files: %d\n", numoffiles);
+        printf("%s\n", outfile);
         //создаем директорию с нужным названием
         //и заходим в нее
-        mkdir(outfile, 
-        S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP|
-        S_IWGRP|S_IROTH|S_IWOTH|S_IXOTH);
+        mkdir(outfile,
+            S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP |
+            S_IWGRP | S_IROTH | S_IWOTH | S_IXOTH);
         chdir(outfile);
         //читаем полученные файлы
-        for(int i = 0; i < numoffiles; i++)
-            ReadCat(in);
+        for (int i = 0; i < numoffiles; i++)
+            ReadCatalog(in);
         //возвращаемся на уровень назад
         chdir("..");
-        
+
     }
     //если это файл - читаем и записываем его
     else
         ReadFromFile(in);
-    
+
 
 }
 
-int dearc(const char* filepath)
+int dearchivate(const char* filepath)
 {
-
-    //char longbuf[255];
-    int in, out;
-    int namesize;
-    int filesize = 0;
-
+    int out;
+    int in;
 
     //открываем архив
     in = open("arcive.arc", O_RDONLY);
-    ReadCat(in);
+    if (out == -1)
+    {
+        printf("Ошибка открытия архива!");
+        return 1;
+    }
+    ReadCatalog(in);
     close(in);
-
-    return 0;
-}
-
-
-int main()
-{
-    int ch;
-    printf("Select action:\n");
-    printf("1 - files to archive\n");
-    printf("2 - read archive\n");
-    scanf("%d", &ch);
-    if(ch == 1)
-        StartArc();
-    else if (ch == 2)
-        dearc("arcive.arc");
 
     return 0;
 }
